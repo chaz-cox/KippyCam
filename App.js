60424ce371f1ee5e9bd2d64f1cd2f5fbce848b8c
chaz-cox <chazcheezecox@gmail.com>
2024-04-05 12:43:03 -0600

    just a little change

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+  import { StyleSheet, Text, View, Image } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  export default function App() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Fetch Image from firebase storage</Text>
+        <Image
+        style={styles.image}
+        source={{
+            uri:"https://firebasestorage.googleapis.com/v0/b/esp32-a1f31.appspot.com/o/data%2Fphoto.jpg?alt=media&token=091fe901-e2d1-41a7-a7e7-f970efe03b65"        
+            }}
+            />
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+      image:{
+          width: 400,
+          height: 300,
+          resizeMode: 'contain',
+          borderRadius: 50,
+      },
+      text:{
+          marginBottom: 15,
+          fontWeight: 'bold',
+          fontSize: 20,
+      },
+  });
+
